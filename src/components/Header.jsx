daa300c8e1a8fbb25a7cd2e0fbf145cbff63783a
dskyberg <davidskyberg@gmail.com>
2021-08-09 15:59:53 -0400

    Updated to show white and black progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from "@material-ui/core/Button";
+import Box from '@material-ui/core/Box'
 import Typography from "@material-ui/core/Typography";
+import PLayer from '../classes/Player'
+import Player from "../classes/Player";
 
 const useStyles = makeStyles(theme => ({
     title: {
@@ -24,12 +27,17 @@ const Header = observer((props) => {
     return (
         <AppBar>
         <Toolbar>
-          <Typography className={classes.title}>
-
-          </Typography>
+          <Box className={classes.title} flexDirection="column">
+            <Typography >Remaining White: {settings.board.remaining[PLayer.WHITE]}</Typography>
+            <Typography >White Kings: {settings.board.kings[PLayer.WHITE]}</Typography>
+          </Box>
           <Typography variant="h6" className={classes.title}>
             {settings.banner}
           </Typography>
+          <Box className={classes.title} flexDirection="column">
+            <Typography >Remaining Black: {settings.board.remaining[Player.BLACK]}</Typography>
+            <Typography >Black Kings: {settings.board.kings[Player.BLACK]}</Typography>
+          </Box>
           <Button color="inherit" onClick={handleNewGame}>New Game</Button>
         </Toolbar>
       </AppBar>
